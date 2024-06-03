d615c30b6d91a867d0321a5aa33dcccf49b81b06
abyanzz <jilclix7@gmail.com>
2024-06-03 16:38:00 +0700

    Update fix 2 repocket_setup.sh

--- repocket_setup.sh
+++ repocket_setup.sh
@@ -3,9 +3,13 @@
 # Set environment variables
 RP_EMAIL="abyantrader@gmail.com"
 RP_API_KEY="cd2cb2ba-38b1-484d-b303-387f15ad4e06"
-PROXY_URL="http://45.195.80.59:5432"
+PROXY_HOST="45.195.80.59"
+PROXY_PORT="5432"
 PROXY_USERNAME="dmqkq"
 PROXY_PASSWORD="tjwpatm9"
+
+# Construct proxy URL with credentials
+PROXY_URL="http://${PROXY_USERNAME}:${PROXY_PASSWORD}@${PROXY_HOST}:${PROXY_PORT}"
 
 # Update system and install Docker
 sudo apt-get update
@@ -24,15 +28,13 @@ sudo docker run -d --name repocket_no_proxy \
   -e RP_API_KEY=$RP_API_KEY \
   repocket/repocket
 
-# Create and run container 2 with proxy
+# Create and run container 2 with proxy and log output to a file
 sudo docker run -d --name repocket_with_proxy \
   -e RP_EMAIL=$RP_EMAIL \
   -e RP_API_KEY=$RP_API_KEY \
   -e HTTP_PROXY="$PROXY_URL" \
   -e HTTPS_PROXY="$PROXY_URL" \
-  -e PROXY_USERNAME=$PROXY_USERNAME \
-  -e PROXY_PASSWORD=$PROXY_PASSWORD \
-  repocket/repocket
+  repocket/repocket > repocket_with_proxy.log 2>&1
 
 # Check the status of the containers
 NO_PROXY_STATUS=$(sudo docker inspect -f '{{.State.Status}}' repocket_no_proxy)
@@ -51,4 +53,6 @@ fi
 if [ "$WITH_PROXY_STATUS" != "running" ]; then
   echo "Logs for container 2 (With Proxy):"
   sudo docker logs repocket_with_proxy
+  echo "Detailed logs for container 2 are saved in repocket_with_proxy.log"
 fi
+
